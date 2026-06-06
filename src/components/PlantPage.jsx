e59ad49a6858cf7dd00a09869e53e026b90919c2
Joel Kamiru <kamirujoel2000@gmail.com>
2026-06-06 21:45:06 +0300

    added functionalities to the plantpage to fetch data, add new plant, filter plants to display

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //Fetch plants 
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  //Handle adding a new plant 
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  //Filter plants based on the search input
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
